e17f6293a510b9b0dad35c1a04efa15b9f0bed05
Marc Fernandez <mfernandez225@gmail.com>
2020-03-17 23:10:59 -0700

    got it running

--- Develop/public/database.js
+++ Develop/public/database.js
@@ -20,29 +20,34 @@ request.onerror = function (event) {
   console.log("Error " + event.target.errorCode);
 };
 
-function checkDatabase() {
+function saveRecord(record) {
   const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.createObjectStore("pending");
-  const getAll = store.getAll();
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
 
-getAll.onsuccess = function () {
-  if (getAll.result.length > 0) {
-    fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        const transaction = db.transaction(["pending"], "readwrite");
-        const store = transaction.objectStore("pending");
-        store.clear();
-      });
-  }
-};
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
 window.addEventListener("online", checkDatabase);
